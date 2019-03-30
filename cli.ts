00a9f5486a59628b7925dfb0bc6ef58ddff428b8
Robert Laverty <roblav96@gmail.com>
2019-03-30 18:40:54 -0400

    init v1

---
+++ cli.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import * as dts from './'
+import * as mri from 'mri'
+
+async function start() {
+	let argv = mri(process.argv.slice(2))
+	let arg = Array.isArray(argv._) && (argv._[0] as string)
+	if (!arg) throw new Error('Input argument is undefined')
+	let pkg = require(arg)
+	let output = await dts(pkg, arg, true)
+	console.log(`\n${output}\n`)
+}
+start().catch(error => console.error(`dts-generate Error ->`, error))
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { FormatCodeSettings } from 'typescript/lib/typescript'
+import startcase = require('lodash.startcase')
+
+function generate(value: any, name = '____', silent = false) {
+	return import('typescript/lib/typescript').then(function({
+		generateTypesForGlobal,
+		getDefaultFormatCodeSettings,
+	}) {
+		let settings = Object.assign(getDefaultFormatCodeSettings(), {
+			convertTabsToSpaces: false,
+		} as FormatCodeSettings)
+		name = startcase(name).replace(/\s+/g, '')
+		let raw = generateTypesForGlobal(name, value, settings)
+		let output = raw.replace(/;/g, '').trim()
+		if (!silent) console.log(`\n${output}\n`)
+		return output
+	})
+}
+
+declare namespace generate {}
+export = generate
