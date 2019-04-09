58575a6acfcfd52a3984f0f6479df1f5ce734c35
Robert Laverty <roblav96@gmail.com>
2019-04-09 11:52:01 -0400

    fixed arrays

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 import { FormatCodeSettings } from 'typescript/lib/typescript'
-import startcase = require('lodash.startcase')
+import * as keys from 'all-keys'
+import { cloneDeep, isArray, isPlainObject, isNull, startCase } from 'lodash'
+
+function fix(value: any) {
+	value &&
+		keys(value).forEach(k => {
+			let v = value[k]
+			if (isArray(v)) {
+				v.forEach(fix)
+			}
+			if (isPlainObject(v)) {
+				fix(v)
+			}
+			if (isNull(v)) {
+				value[k] = undefined
+			}
+		})
+	return value
+}
 
 function generate(value: any, name = '', silent = false) {
 	return import('typescript/lib/typescript').then(function({
@@ -9,10 +27,10 @@ function generate(value: any, name = '', silent = false) {
 		let settings = Object.assign(getDefaultFormatCodeSettings(), {
 			convertTabsToSpaces: false,
 		} as FormatCodeSettings)
-		name = name && startcase(name).replace(/\s+/g, '') || '____'
-		let raw = generateTypesForGlobal(name, value, settings)
+		name = (name && startCase(name).replace(/\s+/g, '')) || '____'
+		let raw = generateTypesForGlobal(name, fix(cloneDeep(value)), settings)
 		let output = raw.replace(/;/g, '').trim()
-		if (!silent) console.log(`\n\n${output}\n\n`)
+		if (!silent) console.log(output)
 		return output
 	})
 }
