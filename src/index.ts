9fd43785dbcf8ae503e433bf8584c7d825d6a950
Robert Laverty <roblav96@gmail.com>
2021-01-11 11:30:10 -0500

    bump@v1.11.0

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import * as _ from 'lodash'
-import * as stringFn from 'string-fn'
-
-function combine(value: any) {
-	if (_.isArray(value) && value.find((v) => _.isPlainObject(v))) {
-		return [_.merge({}, ...value.map((v) => combine(v)))]
-	}
-	if (_.isPlainObject(value) && !_.isArray(value)) {
-		value = _.pickBy(value, (v) => !_.isNil(v))
-		return _.mapValues(value, (v) => combine(v))
-	}
-	return value
-}
-
-/** `typescript` + dependencies are loaded on-demand using `dynamic import()` */
+/** `typescript` + dependencies are loaded on-demand using `await import(...)` */
 export async function generate(value: any, identifier = '') {
-	let { generateIdentifierDeclarationFile } = await import('dts-gen')
+	const _ = await import('lodash')
+	const stringFn = await import('string-fn')
+
+	function combine(value: any) {
+		if (_.isArray(value) && value.find((v) => _.isPlainObject(v))) {
+			return [_.merge({}, ...value.map((v) => combine(v)))]
+		}
+		if (_.isPlainObject(value) && !_.isArray(value)) {
+			value = _.pickBy(value, (v) => !_.isNil(v))
+			return _.mapValues(value, (v) => combine(v))
+		}
+		return value
+	}
+
+	const { generateIdentifierDeclarationFile } = await import('dts-gen')
 	let output = generateIdentifierDeclarationFile(
 		stringFn.pascalCase(identifier) || '____',
 		combine(value),
@@ -22,6 +22,5 @@ export async function generate(value: any, identifier = '') {
 	output = output.replace(/[;]/g, '')
 	output = output.replace(/\015\n\015\n+/g, '\n')
 	output = output.replace(/    /g, '\t')
-	output = output.trim()
-	return output
+	return output.trim()
 }
